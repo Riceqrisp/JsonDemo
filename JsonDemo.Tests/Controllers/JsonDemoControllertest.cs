e2389c07264390529b514474db411980de21c4e4
Riceqrisp <tmoruszewicz@gmail.com>
2019-08-06 08:49:37 +0200

    init2

---
+++ JsonDemo.Tests/Controllers/JsonDemoControllertest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JsonResultDemo.Controllers;
+
+namespace JsonResultDemo.Tests.Controllers
+{
+    [TestClass]
+    public class JsonDemoControllerTest
+    {
+        [TestMethod]
+        public void WelcomeNote()
+        {
+            JsonDemoController controller = new JsonDemoController();
+
+            JsonResult result = controller.WelcomeNote();
+            string msg = Convert.ToString(result.Data);
+            // Assert  
+            Assert.AreEqual("Welcome to the User", msg);
+        }
+    }
+}
